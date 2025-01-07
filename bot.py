b0f8fb07a1f93e39e70bcb34046ccd4e429f8959
nagolse <nagorniyoleksandr6@gmail.com>
2025-01-07 14:41:31 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,19 +6,29 @@ from handlers import time_split, save_user
 from dotenv import load_dotenv
 from db import init_db  
 from fastapi import FastAPI
-
+import threading
 load_dotenv()
 app = FastAPI()
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=6000)
+
+    
 logging.basicConfig(level=logging.INFO)
 
 
 bot = Bot(token=os.getenv("TOKEN"))
 dp = Dispatcher()
 
+async def main():
+    asyncio.run(main())
 
+def run_uvicorn():
+    uvicorn.run(app, host="0.0.0.0", port=6000)
+
+
+async def run_tasks():
+    # Start the database initialization and main task concurrently
+    await asyncio.gather(init_db()
+
+                         
 @dp.message()
 async def cmd_start(message: types.Message):
     await save_user(message)
@@ -30,7 +40,11 @@ async def cmd_start(message: types.Message):
 async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    init_db()
-    asyncio.run(main())
 
+if __name__ == "__main__":
+    # Run the Uvicorn server in a separate thread
+    uvicorn_thread = threading.Thread(target=run_uvicorn)
+    uvicorn_thread.start()
+
+    # Run your asynchronous tasks concurrently
+    asyncio.run(run_tasks())
