754972ef425382d90927e0345d2bd2e589895ec0
nagolse <nagorniyoleksandr6@gmail.com>
2025-01-07 14:22:25 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,8 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=6000)
     init_db()
     asyncio.run(main())
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=6000)
+    
