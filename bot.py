29ab1341d3b2c588262dfe4ab40d9dff57a3303c
nagolse <nagorniyoleksandr6@gmail.com>
2025-01-07 14:09:01 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,6 +29,6 @@ async def main():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=6000)
     init_db()
     asyncio.run(main())
